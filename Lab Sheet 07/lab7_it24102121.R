50e5567022378c8c7e4d39b8131d63c32d62d751
IT24102121 <it24102121@my.sliit.lk>
2025-09-16 12:21:32 +0530

    Add files via upload

---
+++ Lab Sheet 07/lab7_it24102121.R
@@ -0,0 +1,10 @@
+setwd("C:\\Users\\USER\\OneDrive\\Desktop\\it24102121\\lab7_it24102121")
+#01
+punif(25,min = 0,max = 40)-punif(10,min = 0,max = 40)
+#02
+pexp(2, rate = 0.33, lower.tail = TRUE)
+#03
+#1
+1-pnorm(130,mean=100,sd=15)
+#2
+qnorm(0.95,mean=100,sd=15)
